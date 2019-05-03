eecc5ff0573f4f2570e06931c08c865559e69d6c
theonetheycallneo <anthony.wentzel@gmail.com>
2019-05-03 16:02:47 -0700

    fix: poor linkage

---
+++ ios/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  RNAnalyticsIntegration
+//
+//  Created by fathy on 05/08/2018.
+//  Copyright © 2018 Segment.io, Inc. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+#import <RNAnalytics/RNAnalytics.h>
+//#import <segment-appsflyer-ios/SEGAppsFlyerIntegrationFactory.h>
+#import "SEGAppsFlyerIntegrationFactory.h"
+
+@interface RNAnalyticsIntegration_AppsFlyer: NSObject<RCTBridgeModule>
+@end
+
+@implementation RNAnalyticsIntegration_AppsFlyer
+
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(setup) {
+    [RNAnalytics addIntegration:SEGAppsFlyerIntegrationFactory.instance];
+}
+
+@end
